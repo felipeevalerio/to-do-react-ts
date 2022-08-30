8561ff939b77b299ebc9260c437b27c04030754d
Felipe Espindola Valerio <felipe.valerio@dtidigital.com.br>
2022-08-30 15:25:14 -0300

    Alterando nome de concluded para completed

--- src/components/TasksDashboard/TasksDashboard.tsx
+++ src/components/TasksDashboard/TasksDashboard.tsx
@@ -6,17 +6,23 @@ interface TasksDashboardProps {
     tasks: string[];
 }
 
+export interface Task {
+    content: string;
+    completed: boolean;
+}
+
+
 export function TasksDashboard ({ tasks }: TasksDashboardProps) {
     return (
         <div className={styles.tasksDashboard}>
             <header className={styles.info}>
                 <strong className={styles.numberOfCreatedTasks}>Tarefas Criadas <span>0</span></strong>
-                <strong className={styles.numberOfConcludedTasks}>Tarefas Concluídas <span>0</span></strong>
+                <strong className={styles.numberOfCompletedTasks}>Tarefas Concluídas <span>0</span></strong>
             </header>
 
             <ul className={styles.tasksContainer}>
                 {tasks.length > 0 ? tasks.map(task => {
-                    return <Task />
+                    return <Task key={task}/>
                 }) : <EmptyTask />
                 }
             </ul>
